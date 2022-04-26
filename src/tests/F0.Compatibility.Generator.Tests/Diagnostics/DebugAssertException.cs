bfa590342524107ac2c911dd47a376582924678f
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2022-04-26 21:44:14 +0200

    refactor: prefer IdentifierName over QualifiedName

--- src/tests/F0.Compatibility.Generator.Tests/Diagnostics/DebugAssertException.cs
+++ src/tests/F0.Compatibility.Generator.Tests/Diagnostics/DebugAssertException.cs
@@ -1,8 +1,10 @@
 #if DEBUG && NETFRAMEWORK
+using System.Diagnostics.CodeAnalysis;
+
 namespace F0.Tests.Diagnostics;
 
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "Debug.Assert on .NET Framework")]
-[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1064:Exceptions should be public", Justification = "Debug.Assert on .NET Framework")]
+[SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "Debug.Assert on .NET Framework")]
+[SuppressMessage("Design", "CA1064:Exceptions should be public", Justification = "Debug.Assert on .NET Framework")]
 internal sealed class DebugAssertException : Exception
 {
 	public DebugAssertException(string? message)
